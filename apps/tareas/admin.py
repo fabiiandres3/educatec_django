e468c70ffaf30e74680ed8d178666da674581844
myuliet2024@gmail.com <myuliet2024@gmail.com>
2026-06-11 10:18:41 -0500

    por si acaso

--- apps/tareas/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Tareas
-
-# Register your models here.
-
-admin.site.register(Tareas)
--- apps/user/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    nombre = models.CharField('Nombre', max_length=100)
-
-    def __str__(self):
-        return self.nombre if self.nombre else self.username
-
